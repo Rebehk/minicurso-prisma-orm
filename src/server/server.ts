dfe695351acad83306dc18bc883b407651e0ac5b
Railson Mateus <raylsomatheus@hotmail.com>
2023-10-04 11:12:32 -0300

    corrigi server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import http from "http";
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(express.json());
 
-export { server };
+export { app };
